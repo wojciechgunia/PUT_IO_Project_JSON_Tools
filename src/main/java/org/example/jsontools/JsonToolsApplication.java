95f90e91e41f0e8eb80b9c164ff87406efa3222b
Wojciech Gunia <you@example.com>
2024-12-02 15:55:07 +0100

    JTL7-9 Create Spring Project

---
+++ src/main/java/org/example/jsontools/JsonToolsApplication.java
@@ -0,0 +1,15 @@
+package org.example.jsontools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonToolsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JsonToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jsontools/JsonToolsApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.jsontools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonToolsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
