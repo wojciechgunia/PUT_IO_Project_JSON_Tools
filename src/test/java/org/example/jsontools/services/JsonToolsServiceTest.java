8812f0d7116550d4238615a6d20aa64b0d5b339a
KolacinskiP <pawel.kolacinski@student.put.poznan.pl>
2025-01-25 01:44:04 +0100

    JTL7-39-Stworzenie-mock-object

--- src/test/java/org/example/jsontools/services/JsonToolsServiceTest.java
+++ src/test/java/org/example/jsontools/services/JsonToolsServiceTest.java
@@ -1,25 +1,41 @@
 package org.example.jsontools.services;
 
+import org.example.jsontools.entity.Code;
+import org.example.jsontools.entity.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-
+@ExtendWith(MockitoExtension.class)
 class JsonToolsServiceTest {
 
+    @InjectMocks
     private JsonToolsService service;
+
+    @Mock
+    private JsonToolsService mockService;
+
     ResponseEntity<?> response;
     String jsonBody = "{  \"name\":\"John\",\n\t\t \"age\":30,\n \"car\":null}";
     String jsonName = "testJson";
 
     @BeforeEach
     void setUp() {
-        service = new JsonToolsService();
+        MockitoAnnotations.openMocks(this);
+        //service = new JsonToolsService();
         service.saveJSON(jsonBody, jsonName);
     }
 
@@ -150,5 +166,75 @@ class JsonToolsServiceTest {
     void TestgetOriginalListClientError() {
         response = service.getJSON("jsonName2");
         assertTrue(response.getStatusCode().is4xxClientError());
+    }
+
+    @Test
+    void testSaveJSONWithMock() {
+
+        // Configure mock object
+        when(mockService.getJSON(jsonName)).thenReturn(ResponseEntity.ok(new ResponseEntity<>(jsonBody, HttpStatus.OK)));
+
+        // Interaction
+        response = mockService.saveJSON(jsonBody, jsonName);
+
+        // Verification
+        verify(mockService).saveJSON(jsonBody, jsonName);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    void testGetJSONWithMock() {
+
+        // Configure mock object
+        when(mockService.getJSON(jsonName)).thenReturn(ResponseEntity.ok(new ResponseEntity<>(jsonBody, HttpStatus.OK)));
+
+        // Interaction
+        response = mockService.getJSON(jsonName);
+
+        // Verification
+        verify(mockService).getJSON(jsonName);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    void testGetOriginalWithMock() {
+
+        // Configure mock object
+        when(mockService.getOriginal(jsonName)).thenReturn(ResponseEntity.ok(new ResponseEntity<>(jsonBody, HttpStatus.OK)));
+
+        // Interaction
+        response = mockService.getOriginal(jsonName);
+
+        // Verification
+        verify(mockService).getOriginal(jsonName);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    void testFullJSONWithMock() {
+
+        // Configure mock object
+        when(mockService.fullJSON(jsonName)).thenReturn(ResponseEntity.ok(new ResponseEntity<>(jsonBody, HttpStatus.OK)));
+
+        // Interaction
+        response = mockService.fullJSON(jsonName);
+
+        // Verification
+        verify(mockService).fullJSON(jsonName);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    void testMinJSONWithMock() {
+
+        // Configure mock object
+        when(mockService.minJSON(jsonName)).thenReturn(ResponseEntity.ok(new ResponseEntity<>(jsonBody, HttpStatus.OK)));
+
+        // Interaction
+        response = mockService.minJSON(jsonName);
+
+        // Verification
+        verify(mockService).minJSON(jsonName);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
     }
 }
